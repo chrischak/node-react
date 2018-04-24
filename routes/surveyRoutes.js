60a36e102ca778c15e344281839a02e32e9c4b8a
cchak <crazy@localhost.localdomain>
2018-04-24 12:28:37 -0400

    setting up sendGrid configuration

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
@@ -12,9 +14,12 @@ module.exports = app => {
       title,
       subject,
       body,
-      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      recipients: recipients.split(",").map(email => ({ email })),
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
